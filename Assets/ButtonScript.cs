637569a66469562ef2ed7dbbfa041596148266f8
Ivan <sirius9764@gmail.com>
2015-10-24 04:09:12 +0300

    Checkpoint

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+    void OnClick()
+    {
+        if (name == "LevelsButton")
+        {
+            Application.LoadLevel("LevelsMenuScene");
+        }
+        else if (name == "BackToMainMenuButton")
+        {
+            Application.LoadLevel("MainMenuScene");
+        }
+        else if (name == "QuitButton")
+        {
+            Application.Quit();
+        }
+        else if (name == "PlayButton")
+        {
+            Application.LoadLevel("GameScene");
+        }
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
