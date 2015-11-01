8c6704ba69061bdd5a85c60b11af5cfcbda5d858
Ivan <sirius9764@gmail.com>
2015-11-01 14:50:34 +0200

    Sound button was added

---
+++ Assets/SoundButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SoundButton : MonoBehaviour {
+    public Sprite SoundOff;
+    public Sprite SoundOn;
+    public bool Sound;
+    public void MouseClick()
+    {
+        Sound = !Sound;
+        PlayerPrefs.SetInt("Sound", Sound ? 1 : 0);
+        GetComponent<Image>().sprite = Sound ? SoundOn : SoundOff;
+    }
+	// Use this for initialization
+	void Start () {
+        if (!PlayerPrefs.HasKey("Sound"))
+        {
+            PlayerPrefs.SetInt("Sound", 1);
+            Sound = true;
+        }
+        else
+        {
+            Sound = PlayerPrefs.GetInt("Sound") == 1 ?  Sound = true : Sound = false;
+            GetComponent<Image>().sprite = Sound ? SoundOn : SoundOff;
+        }
+	}
+	
+}
